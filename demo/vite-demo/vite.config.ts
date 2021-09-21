0dbd140d79e15de1c60c41cbe04ec3b820f91b65
Jinmay Jhaveri <jinmayjhaveri006@gmail.com>
2021-09-21 22:09:44 +0530

    feat: adding vite-plugin-inspect

--- demo/vite-demo/vite.config.ts
+++ demo/vite-demo/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuelte from "../../src/Plugin";
+import Inspect from "vite-plugin-inspect";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuelte()],
+  plugins: [Inspect(), vuelte(), vue()],
 });
